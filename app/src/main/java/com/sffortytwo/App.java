62369a9ed4907c09d2f1161b5718f8956bb741d8
Jeffrey Johnson <sffortytwo@gmail.com>
2021-10-01 16:27:10 +0200

    Added more bad code

--- app/src/main/java/com/sffortytwo/App.java
+++ app/src/main/java/com/sffortytwo/App.java
@@ -7,14 +7,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class App {
+
+    private static String password = "abc123";
+
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        String username = "scott";
         try {
-            DriverManager.getConnection("", "scott", "tiger");
+            DriverManager.getConnection("", username, password);
         } catch (SQLException sqle) {
             System.out.println("There was an error");
         }
